68ac81df0a87b3d563e88bbb434fdef79adf9185
GRAY <u2227951@live.warwick.ac.uk>
2026-01-30 10:35:41 +0000

    Added initial files with gitignore

--- lab1/vitis/_ide/workspace_journal_16608.py
+++
@@ -1,8 +0,0 @@
-# 2026-01-23T13:58:43.521070700
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="vitis")
-
-comp = client.create_hls_component(name = "platform",cfg_file = ["hls_config.cfg"],template = "empty_hls_component")
-
